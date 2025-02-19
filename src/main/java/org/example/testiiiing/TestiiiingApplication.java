2607be64ad80dfbbc3b8a032577db1ee2debf3af
umaraliyevashoxsanam <jetbrains085@gmail.com>
2025-02-19 18:40:34 +0530

    Initial commit

---
+++ src/main/java/org/example/testiiiing/TestiiiingApplication.java
@@ -0,0 +1,13 @@
+package org.example.testiiiing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestiiiingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestiiiingApplication.class, args);
+    }
+
+}
